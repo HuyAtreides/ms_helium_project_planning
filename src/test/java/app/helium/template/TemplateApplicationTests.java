8bb381010cfb19a9ef11a2a893c442e6811883e4
sophon <phangiahuy2001@gmail.com>
2024-08-26 23:10:52 +0700

    first commit

---
+++ src/test/java/app/helium/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package app.helium.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
