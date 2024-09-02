bb38e36d475a32615ada1036978f9d24b151bacd
sophon <phangiahuy2001@gmail.com>
2024-09-02 14:49:48 +0700

    add create tables sql script

---
+++ src/main/resources/db.migration/V1_0__create_tables.sql
@@ -0,0 +1,74 @@
+set search_path = helium;
+
+create table project_read_only (
+	id uuid primary key,
+	created_at timestamp default (now() at time zone 'utc'),
+	last_updated_at timestamp default (now() at time zone 'utc'),
+	last_updated_by_id uuid not null,
+	project_lead_id uuid null,
+	default_assignee_id uuid null
+);
+
+create table sprint (
+	id uuid primary key,
+	name varchar(200) default 'New Sprint',
+	goal varchar(700) null,
+	created_at timestamp default (now() at time zone 'utc'),
+	last_updated_at timestamp default (now() at time zone 'utc'),
+	last_updated_by_id uuid not null,
+	start_date timestamp,
+	end_date timestamp,
+	project_id uuid references project_read_only(id)
+);
+
+create table issue_label_relationship (
+	label_id varchar(500),
+	issue_id uuid,
+	primary key (label_id, issue_id)
+);
+
+create table issue_label (
+	id uuid primary key,
+	label varchar(500) not null,
+	project_id uuid references project_read_only(id)
+)
+
+create table issue (
+	id uudi primary key,
+	summary varchar(500) not null,
+	description varchar(1000) null,
+	attachment_urls text[]  default ARRAY[],
+	created_at timestamp default (now() at time zone 'utc'),
+	last_updated_at timestamp default (now() at time zone 'utc'),
+	last_updated_by_id uuid not null,
+	label varchar(500) null references issue_label_relationship (label),
+	point_estimate int not null,
+	start_date timestamp,
+	due_date timestamp,
+	project_id uuid references project_read_only(id),
+	sprint_id uuid references sprint(id)
+);
+
+create table issue_status (
+	id uuid primary key,
+	name varchar(200) not null,
+	description varchar(1000) null,
+	custom boolean not null default false,
+	project_id uuid references project_read_only(id),
+	issue_id uuid references issue(id)
+);
+
+create table issue_type (
+	id uuid primary key,
+	name varchar(200) not null,
+	description varchar(1000) null,
+	icon_url varchar(200) not null,
+	custom boolean not null default false,
+	project_id uuid references project_read_only(id),
+	issue_id uuid references issue(id)
+);
+
+
+
+
+
